3798a5965b437d762d702299d62298bce1b787eb
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2021-12-12 06:33:53 +0000

    Sync LeetCode submission - Single Number (cpp)

---
+++ LeetCode/problems/single_number/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        if(nums.size()==1) 
+            return nums[0];
+        for(int i=1;i<nums.size();i++)
+            nums[0]^=nums[i];
+        
+        return nums[0];
+    }
+};
