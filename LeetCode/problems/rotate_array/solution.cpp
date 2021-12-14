7b46bf0d6ac1d464b50c55612a408d6edc06de71
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2021-12-14 05:48:20 +0000

    Sync LeetCode submission - Rotate Array (cpp)

---
+++ LeetCode/problems/rotate_array/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        if (k==0) 
+            return;
+        if (nums.size()==0) 
+            return;
+   
+        int* res = new int[nums.size()];
+
+        for (int i=0; i<nums.size(); i++) 
+        {
+        int newIndex = (i + k) % nums.size();
+        res[newIndex] = nums[i];
+        }
+   
+   //assign back to original array
+        for (int i=0; i<nums.size(); i++) 
+        {
+        nums[i] = res[i];
+        }
+    }
+};
