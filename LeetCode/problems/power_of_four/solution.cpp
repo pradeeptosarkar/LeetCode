a22b7688f443cdf1d2c40037aea53730fa169c82
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2021-12-21 06:09:17 +0000

    Sync LeetCode submission - Power of Four (cpp)

---
+++ LeetCode/problems/power_of_four/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) 
+    {
+        if(n<=0)
+            return false;
+            
+        if(n==1)
+            return true;
+        
+        if(n & n-1)
+            return false;
+        
+        int c;
+        
+        while(!(n&1))
+        {
+            c++;
+            n=n>>1;
+        }
+        
+        if((c&1)==0)
+            return true;
+        
+        return false;
+    }
+};
