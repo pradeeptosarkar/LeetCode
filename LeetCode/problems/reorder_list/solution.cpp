e2f1a4c588eaf5bed687073dbccb6b3216790301
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2021-12-22 04:26:23 +0000

    Sync LeetCode submission - Reorder List (cpp)

---
+++ LeetCode/problems/reorder_list/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        //base case i.e if the linked list has zero,one or two elments just return it
+        if(!head || !head->next || !head->next->next) return;
+        
+        //Find the penultimate node i.e second last node of the linkedlist
+        ListNode* penultimate = head;
+        while (penultimate->next->next) penultimate = penultimate->next;
+        
+        // Link the penultimate with the second element
+        penultimate->next->next = head->next;
+        head->next = penultimate->next;
+        
+        //Again set the penultimate to the the last 
+        penultimate->next = NULL;
+        
+        // Do the above steps rcursive
+        reorderList(head->next->next);
+    }
+};
