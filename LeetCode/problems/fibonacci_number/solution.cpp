3b09952148a3ec4c48623f6cd317634bedb8d8af
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-01-06 08:27:09 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- LeetCode/problems/fibonacci_number/solution.cpp
+++ LeetCode/problems/fibonacci_number/solution.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     int fib(int n) 
     {
-        if(n==0)
-            return 0;
-        if(n==1)
-            return 1;
+        int fibo[31];
+        fibo[0]=0;
+        fibo[1]=1;
         
-        int ans=fib(n-1)+fib(n-2);
-        return ans;
+        if(n==0 || n==1)
+            return fibo[n];
+        
+        else
+        {
+            for(int i=2;i<=n;i++)
+                fibo[i]=fibo[i-1]+fibo[i-2];
+        }
+        
+        return fibo[n];
     }
 };
