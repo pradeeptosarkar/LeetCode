121317bbcfef23c721c5bccd1fe92bf67a7bb64b
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-01-11 07:21:10 +0000

    Sync LeetCode submission - Minimum Absolute Difference in BST (cpp)

---
+++ LeetCode/problems/minimum_absolute_difference_in_bst/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+        int prev=-1;
+        int max = INT_MAX;
+        void inorder(TreeNode* root){
+        if(!root)
+            return ;
+     
+        inorder(root->left);
+            
+            if(prev==-1)
+                ;
+            else if(root->val-prev<max)
+                max = root->val-prev;
+            
+         prev = root->val;
+        inorder(root->right);
+
+    }
+    
+    int getMinimumDifference(TreeNode* root) {
+         inorder(root);
+        
+        return max;
+    }
+
+};
