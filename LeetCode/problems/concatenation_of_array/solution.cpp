83ab90de43c3cf839539cb84b5fe4afc93ac0908
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-01-16 07:18:54 +0000

    Sync LeetCode submission - Concatenation of Array (cpp)

---
+++ LeetCode/problems/concatenation_of_array/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int> ans(2*n);
+        
+        for(int i=0;i<n;i++)
+        {
+            ans[i]=nums[i];
+            ans[i+n]=ans[i];
+        }
+        return ans;
+    }
+};
