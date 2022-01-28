7b5845a2dacffa011c4ec95af7a2016c63d5484f
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-01-28 09:03:21 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (cpp)

--- LeetCode/problems/populating_next_right_pointers_in_each_node/solution.cpp
+++ LeetCode/problems/populating_next_right_pointers_in_each_node/solution.cpp
@@ -1,24 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
 class Solution {
 public:
-    Node* connect(Node* root) {
-        if(root == nullptr) return nullptr;
-        std::queue<Node*> curr_Q;
-        std::queue<Node*> nxt_Q;
-        curr_Q.push(root);
-        while(!curr_Q.empty()){
-            Node* n = curr_Q.front();
-            curr_Q.pop();
-            if(n->left != nullptr){
-                nxt_Q.push(n->left);
-                nxt_Q.push(n->right);
-            }
-            if(curr_Q.size() == 0){
-                n->next = nullptr;
-                std::swap(curr_Q, nxt_Q);
-            }
-            else
-                n->next = curr_Q.front();
-        }
+    Node* connect(Node* root) 
+    {
+        if(root==nullptr)
+            return nullptr;
+        helper(root->left,root->right,root->left,root->right);
         return root;
+        
+    }
+    
+    void helper(Node* l, Node* r, Node* l2, Node* r2)
+    {
+        if(l&&r)
+        {
+            l->next=r;
+            helper(l->left,l->right,l->left,l->right);
+            helper(r->left,r->right,r->left,r->right);
+        }
+        if(l2&&r2)
+        {
+            l2->next=r2;
+            helper(nullptr,nullptr,l2->right,r2->left);
+        }
     }
 };
