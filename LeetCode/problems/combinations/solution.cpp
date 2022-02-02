6ad6f32e57ee6248f6c7621e79933a93f4fabacd
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-02-02 06:24:58 +0000

    Sync LeetCode submission - Combinations (cpp)

--- LeetCode/problems/combinations/solution.cpp
+++ LeetCode/problems/combinations/solution.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> combine(int n, int k) 
+    {
         vector<bool> used(n,0);
-        vector<vector<int> > res;
-        vector<int>tmp;
-        solve(1,k,n, used,res,tmp);
-        return res;
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(1,k,n,used,ans,temp);
+        return ans;        
     }
-    void solve( int idx,int k , int n , vector<bool>&used, vector<vector<int> >&res 
-    ,vector<int> &tmp ){
-        if(tmp.size()==k){
     
-            res.push_back(tmp);
-            return ;
+    void solve(int idx,int k,int n,vector<bool>&used,vector<vector<int>>&ans, vector<int>&temp)
+    {
+        if(temp.size()==k)
+        {
+            ans.push_back(temp);
+            return;
         }
-        for(int i=idx;i<=n;i++){
-            if(!used[i]){
+        
+        for(int i=idx;i<=n;i++)
+        {
+            if(!used[i])
+            {
                 used[i]=1;
-                tmp.push_back(i); // here we are using simple permutation concepts-> lets say we have 
-                // k places  - - - - , so in first place we have n options then for second we are left with n-1 options then n-2 and so on .... till k  
-                solve(i+1,k,n,used,res,tmp);
+                temp.push_back(i);
+                solve(i+1,k,n,used,ans,temp);
                 used[i]=0;
-                tmp.pop_back();
+                temp.pop_back();
             }
         }
     }
