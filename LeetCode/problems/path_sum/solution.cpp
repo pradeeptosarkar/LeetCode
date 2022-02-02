13652302ed4da642898351c8eaed80f0fb7618d9
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-02-02 07:56:57 +0000

    Sync LeetCode submission - Path Sum (cpp)

---
+++ LeetCode/problems/path_sum/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) 
+    {
+        if(root==NULL)
+            return false;
+        targetSum=targetSum-root->val;
+        
+        if(targetSum==0 and !root->left and !root->right)
+            return true;
+        
+        return hasPathSum(root->right,targetSum) or hasPathSum(root->left,targetSum);
+        
+    }
+};
