d573f70d38dbcf1b8541f98b011c04e673e1a324
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-02-26 08:37:44 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- LeetCode/problems/majority_element/solution.cpp
+++ LeetCode/problems/majority_element/solution.cpp
@@ -1,7 +1,10 @@
+//::Pradeepto Sarkar LC169 26/02/2022:://
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-      nth_element(nums.begin(), nums.begin() + (nums.size() / 2), nums.end());
-	return *(nums.begin() + (nums.size() / 2));
+        
+        nth_element(nums.begin(), nums.begin()+(nums.size()/2), nums.end());
+	    return *(nums.begin() + (nums.size()/2));
     }
 };
