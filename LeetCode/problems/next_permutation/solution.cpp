92b6fa56ece23095a1a074c752214080d6869638
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-04-03 05:01:00 +0000

    Sync LeetCode submission - Next Permutation (cpp)

--- LeetCode/problems/next_permutation/solution.cpp
+++ LeetCode/problems/next_permutation/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
+    void nextPermutation(vector<int>& nums) 
+    {
         int n = nums.size();
 	int idx = n-1;
 
-	// Step 1
-	while(idx >= 1) {
-		if(nums[idx] > nums[idx-1]) {
-			// Step 2
+	while(idx >= 1) 
+    {
+		if(nums[idx] > nums[idx-1]) 
+        {
 			reverse(nums.begin()+idx, nums.end());
 
-			// Step 3
 			for(int i=idx; i<n; i++) {
 				if(nums[i] > nums[idx-1]) {
 					swap(nums[i], nums[idx-1]);
@@ -22,10 +22,7 @@ public:
 		idx--;
 	}
 
-	// Step 4
 	if(idx == 0) 
 		reverse(nums.begin(), nums.end());
-	
-
     }
 };
