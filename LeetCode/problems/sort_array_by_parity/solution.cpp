fdc8a0324bc1d942764a8ef05e16b17c695f568f
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-05-02 15:45:29 +0000

    Sync LeetCode submission - Sort Array By Parity (cpp)

---
+++ LeetCode/problems/sort_array_by_parity/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+      
+    vector<int> sortArrayByParity(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]%2==0)
+                swap(nums[i],nums[j]), j++;
+        }
+        return nums;
+        
+        
+    }
+};
