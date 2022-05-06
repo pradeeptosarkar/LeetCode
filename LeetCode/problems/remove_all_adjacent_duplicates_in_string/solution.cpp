55ba964dd6cb5db6a30f428d45bc9ea9b863a7c9
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-05-06 11:08:44 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates In String (cpp)

---
+++ LeetCode/problems/remove_all_adjacent_duplicates_in_string/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeDuplicates(string s) 
+    {
+        stack<char> stk;
+        
+        for(auto i:s)
+        {
+            if(stk.empty())
+                stk.push(i);
+            
+            else
+            {
+                if(stk.top()==i)
+                    stk.pop();
+                else
+                    stk.push(i);
+            }
+        }
+        string ans="";
+        
+        while(!stk.empty())
+        {
+            ans=stk.top()+ans;
+            stk.pop();
+        }
+        return ans;
+        
+        
+    }
+};
