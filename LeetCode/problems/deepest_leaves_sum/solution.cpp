4321817642da41d89d3ca37deb054b911947245b
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-05-15 10:55:18 +0000

    Sync LeetCode submission - Deepest Leaves Sum (cpp)

--- LeetCode/problems/deepest_leaves_sum/solution.cpp
+++ LeetCode/problems/deepest_leaves_sum/solution.cpp
@@ -1,18 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-	pair<int,int> getSum(TreeNode* root){
-        if(!root) return {0,0};
-        if(!root->left && !root->right) return {1, root->val};
-        auto left = getSum(root->left);
-        auto right = getSum(root->right);
-        if(left.first == right.first)
-            return {left.first+1, left.second + right.second};
-        if(left.first > right.first)
-            return {left.first+1, left.second};
-        return {right.first+1, right.second}; 
+    int deepestLeavesSum(TreeNode* root) 
+    {
+        if(root->left==NULL and root->right==NULL)
+            return root->val;
+        
+        queue<TreeNode*>q;
+        q.push(root);
+        q.push(NULL);
+        int sum=0;
+        int last_sum=0;
+        
+        while(!q.empty())
+        {
+            TreeNode* node=q.front();
+            q.pop();
+            
+            if(node==NULL)
+            {
+                if(q.size()==0)
+                {
+                    last_sum=sum;
+                    break;
+                }
+                q.push(NULL);
+                sum=0;
+            }
+            
+            else
+            {
+                sum+=node->val;
+                if(node->left!=NULL)
+                    q.push(node->left);
+                if(node->right!=NULL)
+                    q.push(node->right);
+            }
+        }
+        return last_sum;
+        
     }
-
-    int deepestLeavesSum(TreeNode* root) {
-        return getSum(root).second;
-    }	
 };
