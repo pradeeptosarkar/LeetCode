a7a17e5347cf808a3dec8c6c70febd25dea52a69
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-05-16 05:58:00 +0000

    Sync LeetCode submission - Plus One (cpp)

--- LeetCode/problems/plus_one/solution.cpp
+++ LeetCode/problems/plus_one/solution.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int i, j = 1, carry = 0, sum = 0, first;
-        vector<int> vect;
-        i = digits.size() - 1;
-        
-        while(i != -1 || j != 0 || carry){
-            if(i != -1) first = digits[i--];
-            else first = 0;
-            
-            sum =  first + j + carry;
-            vect.push_back(sum % 10);
-            carry = sum / 10;
-            j = 0;
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            else{
+                digits[i]=0;
+            }
         }
-        reverse(vect.begin(), vect.end());
-        return vect;
+        digits.push_back(0);
+        digits[0]=1;
+        return digits;
     }
 };
