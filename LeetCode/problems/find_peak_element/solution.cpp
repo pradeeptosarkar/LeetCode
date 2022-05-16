ae5c842b890ea6eb1800fbed24da1152dadefd27
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-05-16 05:48:06 +0000

    Sync LeetCode submission - Find Peak Element (cpp)

--- LeetCode/problems/find_peak_element/solution.cpp
+++ LeetCode/problems/find_peak_element/solution.cpp
@@ -2,35 +2,16 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) 
     {
-        int ans=0;
-        
-        if(nums.size()==1)
-            return 0;
-        
-        else if(nums.size()==2)
+        int l=0, r=nums.size()-1;
+        while(l<r)
         {
-            if(nums[0]>nums[1])
-                return 0;
-            
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[mid+1])
+                r=mid;
             else
-                return 1;
+                l=mid+1;
         }
+        return l;
         
-        else
-        {
-            if(nums[0]>nums[1])
-                ans=0;
-            else if(nums[nums.size()-1]>nums[nums.size()-2])
-                ans=nums.size()-1;
-            else{
-        for(int i=1;i<nums.size()-1;i++)
-        {
-            if(nums[i]>nums[i+1] && nums[i]>nums[i-1])
-            {ans=i;
-            }
-        }
-        }
-        }
-        return ans;
     }
 };
