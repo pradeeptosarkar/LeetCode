7dc5548f267d1bc2b4d7a37e8efb75c247c11cda
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-05-22 04:07:56 +0000

    Sync LeetCode submission - Palindromic Substrings (cpp)

---
+++ LeetCode/problems/palindromic_substrings/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int countPal(string s, int i, int j)
+    {
+        int count=0;
+        while(i>=0 and j<s.size() and s[i]==s[j])
+            i--, j++, count++;
+        
+        return count;
+    }
+    
+    int countSubstrings(string s) 
+    {
+        int ans=0;
+        for(int i=0; i<s.size(); i++)
+            ans+= countPal(s, i, i) + countPal(s, i, i+1);
+        
+        return ans;
+        
+    }
+};
