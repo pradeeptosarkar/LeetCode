3bab7affd36510b0dd3d5df8749dc202c3955189
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-05-25 16:02:32 +0000

    Sync LeetCode submission - The Number of Weak Characters in the Game (cpp)

---
+++ LeetCode/problems/the_number_of_weak_characters_in_the_game/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    static bool comp(vector<int>&a, vector<int>&b)
+    {
+        if(a[0]==b[0])
+            return a[1]>b[1];
+        
+        else
+            return a[0]<b[0];
+    }
+    
+    int numberOfWeakCharacters(vector<vector<int>>& properties) 
+    {
+        sort(properties.begin(), properties.end(), comp);
+        int maxi=0;
+        int ans=0;
+        
+        for(int i=properties.size()-1;i>=0;i--){
+            if(properties[i][1]<maxi)
+                ans++;
+        
+        maxi=max(maxi,properties[i][1]);
+            }
+        return ans;
+        
+    }
+};
