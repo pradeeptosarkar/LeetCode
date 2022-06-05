083fc26d0afdda6f4ce1d613fd39b10399446334
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-06-05 16:28:47 +0000

    Sync LeetCode submission - N-Queens II (cpp)

--- LeetCode/problems/n-queens_ii/solution.cpp
+++ LeetCode/problems/n-queens_ii/solution.cpp
@@ -1,73 +1,19 @@
 class Solution {
 public:
-void blockPlaces(int mat[][10], int n, int x, int y)
-{
-	int i = x+1, j = y+1, k=y;
-
-	while (i < n)
-		mat[i++][y] += 1;
-
-	while (j < n)
-		mat[x][j++] += 1;
-
-	i = x+1, k = y-1;
-
-	while (i < n && k >= 0)
-		mat[i++][k--] += 1;
-
-	i = x+1, j = y+1;
-
-	while (i < n && j < n )
-		mat[i++][j++] += 1;
-}
-
-void unblockPlaces(int mat[][10], int n, int x, int y)
-{
-	int i = x + 1, j = y + 1, k = y;
-
-	while (i < n)
-		mat[i++][y] -= 1;
-
-	while (j < n)
-		mat[x][j++] -= 1;
-
-	i = x + 1, k = y - 1;
-
-	while (i < n && k >= 0)
-		mat[i++][k--] -= 1;
-
-	i = x + 1, j = y + 1;
-
-	while (i < n && j < n)
-		mat[i++][j++] -= 1;
-}
-
-bool N_Queen(int mat[][10],int n,int i, int &count){
-
-	//base case
-	if(i==n){
-        count++;
-
-		return true;
-	}
-
-	for(int j=0;j<n;j++){
-		if (mat[i][j] == 0)
-		{
-			mat[i][j] = -1;
-			blockPlaces(mat, n, i, j);
-
-			N_Queen(mat, n, i + 1, count);
-			mat[i][j] = 0;
-			unblockPlaces(mat, n, i, j);
-		}
-	}
-	return false;
-}
     int totalNQueens(int n) {
-        int mat[10][10] = {0};
-        int count =0;
-        N_Queen(mat,n,0,count);
-        return count;
-    }
+	vector<bool> col(n), diag(2*n-1), anti_diag(2*n-1);
+	return solve(col, diag, anti_diag, 0);
+}
+   
+int solve(vector<bool>& col, vector<bool>& diag, vector<bool>& anti_diag, int row) {
+	int n = size(col), count = 0;
+    if(row == n) return 1;
+	for(int column = 0; column < n; column++)           
+		if(!col[column] && !diag[row + column] && !anti_diag[row - column + n - 1]){ 
+			col[column] = diag[row + column] = anti_diag[row - column + n - 1] = true;
+			count += solve(col, diag, anti_diag, row + 1); 
+			col[column] = diag[row + column] = anti_diag[row - column + n - 1] = false; 
+		}                                
+	return count;
+}
 };
