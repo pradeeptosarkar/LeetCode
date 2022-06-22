e533257845ea5d15385a56e53e5eb7979bf3b665
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-06-22 05:12:51 +0000

    Sync LeetCode submission - Third Maximum Number (cpp)

--- LeetCode/problems/third_maximum_number/solution.cpp
+++ LeetCode/problems/third_maximum_number/solution.cpp
@@ -2,11 +2,18 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) 
     {
-        sort(nums.begin(),nums.end());
-nums.erase(unique(nums.begin(),nums.end()),nums.end());
-if(nums.size()<3)
-return nums[nums.size()-1];
-else
-return nums[nums.size()-3];
+        sort(nums.begin(), nums.end(), greater<int>());
+        deque<int> dq;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(dq.front()==nums[i])
+                continue;
+            dq.push_front(nums[i]);
+            
+            if(dq.size()==3){return dq.front();}
+        }
+        return dq.back();
+        
     }
 };
