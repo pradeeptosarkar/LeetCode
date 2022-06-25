b6f234d5e77144cbf0674cfda2d8aa2492091e0a
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-06-25 07:01:30 +0000

    Sync LeetCode submission - Non-decreasing Array (cpp)

--- LeetCode/problems/non-decreasing_array/solution.cpp
+++ LeetCode/problems/non-decreasing_array/solution.cpp
@@ -1,14 +1,35 @@
-class Solution {
+class Solution 
+{
 public:
-    bool checkPossibility(vector<int>& nums) {
-        int cnt = 0;             
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] < nums[i-1]){
-                if(++cnt > 1) return false;
-                if(i == 1 || nums[i-2] <= nums[i]) nums[i-1] = nums[i];    
-                else nums[i] = nums[i-1];
+	
+    bool isIncreasing(vector<int> nums) 
+    {
+        for(int i=1;i<nums.size();i++) 
+            if(nums[i]<nums[i-1]) return false;
+        
+        return true;
+    }
+    
+    bool checkPossibility(vector<int>& nums) 
+    {
+        for(int i=1;i<nums.size();i++) 
+        {
+            if(nums[i]<nums[i-1]) 
+            {
+                int x=nums[i], y=nums[i-1];
+				nums[i]=y;
+                
+                if(isIncreasing(nums)) 
+                    return true;
+                else 
+                {
+					nums[i]=x;
+                    nums[i-1]=x;
+                    return isIncreasing(nums);
+                }
             }
         }
+        
         return true;
-    } 
+    }
 };
