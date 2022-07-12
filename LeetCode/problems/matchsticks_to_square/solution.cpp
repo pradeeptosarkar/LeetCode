618798640c22a4e4d0b445ac4a24c0cb6a1d496d
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-07-12 06:26:24 +0000

    Sync LeetCode submission - Matchsticks to Square (cpp)

--- LeetCode/problems/matchsticks_to_square/solution.cpp
+++ LeetCode/problems/matchsticks_to_square/solution.cpp
@@ -1,32 +1,49 @@
 class Solution {
 public:
-    long long int mask;
     
-    bool solve(vector<int> &matchsticks, int target, long long mask, int currSum, int i, int k){
-        if(k==1) return true; //If the sum is multiple of 4 and already 3 sides are built, then 4th side can be built.
+    bool solve(vector<int>&matchsticks, int target, vector<int>&ans, int id)
+    {
+        if(id==matchsticks.size())
+            return(ans[0]==ans[1] and ans[1]==ans[2] and ans[2]==ans[3]);
         
-        if(currSum==target) 
-                return solve(matchsticks, target, mask, 0, 0, k-1);  //If we have built a side, set back the currSum to '0' and start building the next side. 
-        
-        for(int j=i; j<matchsticks.size(); j++){ //j=i because while building a side, we start traversing the array from where we left off
+        for(int i=0;i<4;i++)
+        {
+            if(ans[i]+matchsticks[id]>target)
+                continue;
             
-            if((mask&(1<<j))==0 || currSum+matchsticks[j]>target) continue; //If the element is already visited or it's causing the currSum to exceed the target, don't use the element.
+            int j=i-1;
             
-            mask=mask&~(1<<j);  //If not, mark it as visited and continue.
-            if(solve(matchsticks, target, mask, currSum+matchsticks[j], j+1, k))  //continue traversing the other elements with the currSum
+            while(j>=0)
+            {
+                if(ans[i]==ans[j])
+                    break;
+                j--;
+            }
+            
+            if(j!=-1)
+                continue;
+            
+            ans[i]+=matchsticks[id];
+            if(solve(matchsticks,target,ans,id+1))
                 return true;
-            mask=mask|(1<<j); //If we returned with a false, that means the curr_element is not working
             
+            ans[i]-=matchsticks[id];
         }
+        
         return false;
     }
     
-    bool makesquare(vector<int>& matchsticks) {
-        int sum=0;
-        sum = accumulate(matchsticks.begin(), matchsticks.end(), sum);
-        if(matchsticks.size()<4||sum%4) return false; //If the elements don't sum to aq multiple of 4, we can't make a square.
+    bool makesquare(vector<int>& matchsticks) 
+    {
+        if(matchsticks.size()==0)
+            return false;
+        int sum=accumulate(matchsticks.begin(),matchsticks.end(),0);
+        if(sum%4!=0) 
+            return false;
+        int target=sum/4;
+        vector<int> ans(4,0);
+        sort(matchsticks.begin(),matchsticks.end(),greater<int>());
+        return solve(matchsticks,target,ans,0);
         
-        mask=(1<<matchsticks.size())-1; //Initially mask=1111... (no. of elements in the array) represents that none of the elements are visited
-        return solve(matchsticks, sum/4, mask, 0, 0, 4); //k=4 is the number of sides to build.
     }
 };
