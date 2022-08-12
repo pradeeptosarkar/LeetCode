382b7aa7843b9a2096e197716448be7c09dfd04a
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-08-12 06:14:55 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Search Tree (cpp)

--- LeetCode/problems/lowest_common_ancestor_of_a_binary_search_tree/solution.cpp
+++ LeetCode/problems/lowest_common_ancestor_of_a_binary_search_tree/solution.cpp
@@ -10,13 +10,13 @@
 
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root->val > p->val && root->val > q->val){
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        if (root->val > p->val && root->val > q->val)
             return lowestCommonAncestor(root->left, p, q);
-        }
-        if (root->val < p->val && root->val < q->val){
+        
+        if (root->val < p->val && root->val < q->val)
             return lowestCommonAncestor(root->right, p, q);
-        }
         
         return root;
     }
