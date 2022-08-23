33ac80b47963547471508fe89f80f8ffde825c27
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-08-23 05:20:26 +0000

    Sync LeetCode submission - Palindrome Linked List (cpp)

--- LeetCode/problems/palindrome_linked_list/solution.cpp
+++ LeetCode/problems/palindrome_linked_list/solution.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) 
-    {
-         ListNode *slow = head, *fast = head;
-        
-        while(fast!=NULL && fast->next!=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        
-        if(fast != NULL){
-            //the length of list is odd
-            //in this situation we should ignore the middle node
-            slow = slow->next;
-        }
-        
-        //slow: later part of original list
-        //fast: former part of original list
-        slow = reverse(slow);
-        fast = head;
-        
-        while(slow != NULL){
-            if(slow->val != fast->val) return false;
-            slow = slow->next;
-            fast = fast->next;
-        }
-        
-        return true;
-    }
     
-    ListNode* reverse(ListNode* head){
-        //reverse the later part of original list
-        ListNode* prev = NULL;
-        ListNode* tmp;
-        while(head != NULL){
-            tmp = head->next;
-            head->next = prev;
-            prev = head;
-            head = tmp;
+    ListNode * reverse(ListNode *head)
+    {
+        ListNode *prev=NULL;
+        ListNode *temp;
+        
+        while(head!=NULL)
+        {
+            temp=head->next;
+            head->next=prev;
+            prev=head;
+            head=temp;
         }
-        //when the loop finishes, head is NULL, 
-        //and prev is the last node in the original list
         return prev;
     }
+    
+    bool isPalindrome(ListNode* head) 
+    {
+        ListNode *slow=head, *fast=head;
+        
+        while(fast!=NULL and fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        if(fast!=NULL)
+            slow=slow->next;
+        
+        slow=reverse(slow);
+        fast=head;
+        
+        while(slow!=NULL)
+        {
+            if(slow->val!=fast->val)
+                return false;
+            fast=fast->next;
+            slow=slow->next;
+        }
+        
+        return true;    
+    }
+    
 };
