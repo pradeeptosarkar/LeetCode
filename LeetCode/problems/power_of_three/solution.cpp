a960ea53eb5a2bc328667deb9c022bdeb1125698
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-08-24 04:46:34 +0000

    Sync LeetCode submission - Power of Three (cpp)

--- LeetCode/problems/power_of_three/solution.cpp
+++ LeetCode/problems/power_of_three/solution.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     bool isPowerOfThree(int n) 
     {
-        if(n==0)
-            return 0;
-        while(n%3==0)
-        {
-            n/=3;
-        }
-        if(n==1)
-            return 1;
-        return 0;
+        return (n > 0 && 1162261467 % n == 0);
     }
 };
