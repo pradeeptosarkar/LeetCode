32460c393b0cac431b4e3a9dc893afb5eb8c7778
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-08-24 18:04:09 +0000

    Sync LeetCode submission - Stone Game IV (cpp)

--- LeetCode/problems/stone_game_iv/solution.cpp
+++ LeetCode/problems/stone_game_iv/solution.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    int dp[100001];
-    
-    bool solve(int n)
-    {
-        if(n==0)
-            return false;
-        
-        if(dp[n]!=-1)
-            return dp[n];
-        
-        bool flag=false;
-        
-        for(int i=1;i*i<=n;++i)
-            flag|=!solve(n-(i*i));
-        
-        return dp[n]=flag;
-    }
     bool winnerSquareGame(int n) 
     {
-    
-        memset(dp,-1,sizeof(dp));
-        return solve(n);
+        vector<bool> dp(n+1,0);
+        
+        for(int i=1;i<=n;i++)
+            for(int j=1;j*j<=i;j++)
+                if(!dp[i-j*j]) { dp[i]=true; break;}
+        
+        return dp[n];
+        
     }
 };
