9ad8e2c36f88da2f3367ee5824653663e3e931d5
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-08-25 15:04:52 +0000

    Sync LeetCode submission - Ransom Note (cpp)

--- LeetCode/problems/ransom_note/solution.cpp
+++ LeetCode/problems/ransom_note/solution.cpp
@@ -5,13 +5,14 @@ public:
     {
         vector<int> count(26);
         
-        for(char c : magazine){
+        for(char c : magazine)
             count[c-'a'] += 1;
-        }
         
-        for(char c : ransomNote){
+        for(char c : ransomNote)
+        {
             count[c-'a'] -= 1;
-            if(count[c-'a'] < 0) return false;
+            if(count[c-'a'] < 0) 
+                return false;
         }
         
         return true;
