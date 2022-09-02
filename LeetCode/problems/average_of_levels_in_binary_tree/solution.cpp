3b1296f7ee55aeb00408ea903de3f5d43268c95e
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-09-02 05:02:36 +0000

    Sync LeetCode submission - Average of Levels in Binary Tree (cpp)

--- LeetCode/problems/average_of_levels_in_binary_tree/solution.cpp
+++ LeetCode/problems/average_of_levels_in_binary_tree/solution.cpp
@@ -11,45 +11,30 @@
  */
 class Solution {
 public:
-    vector<double> averageOfLevels(TreeNode* root) {
-        vector <vector<int>> ret;
-        if(root == NULL){
-            vector<double> v;
-            return v;
-        } 
-        queue <TreeNode*> q;
+    vector<double> averageOfLevels(TreeNode* root) 
+    {
+        vector<double> ans;
+        queue<TreeNode*> q;
         q.push(root);
-        vector <int> dummy;
-        dummy.push_back(root->val);
-        ret.push_back(dummy);
         
-        // bfs level order
-        while(q.size() != 0){
-            int size = q.size();
-            dummy.clear();
-            for(int i = 0; i < size; i++){
-                TreeNode *curr = q.front();
-                q.pop();
-                if(curr->left){
-                    q.push(curr->left);
-                    dummy.push_back(curr->left->val);
-                }
-                if(curr->right){
-                    q.push(curr->right);
-                    dummy.push_back(curr->right->val);
-                }
-            }
-            if(dummy.size() != 0) ret.push_back(dummy);
-        }
-        
-        // get avg for every array in 2d array
-        vector <double> v;
-        for(int i = 0; i < ret.size(); i++){
-            double s = 0;
-            for(int j = 0; j < ret[i].size(); j++)s += ret[i][j];
+        while(!q.empty())
+        {
+            int s=q.size();
+            long long temp = 0*1LL;
             
-            v.push_back(s / ret[i].size());
+            for(int i=0;i<s;i++)
+            {
+                TreeNode* t=q.front();
+                q.pop();
+                temp+=t->val;
+                if(t->left)
+                    q.push(t->left);
+                if(t->right)
+                    q.push(t->right);
+            }
+            ans.push_back((double)temp/s);
         }
-        return v;
+        return ans;
+        
     }
 };
