c1daff882d46dc190d4b96672446e46b3d07c830
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-09-18 06:05:05 +0000

    Sync LeetCode submission - Trapping Rain Water (cpp)

--- LeetCode/problems/trapping_rain_water/solution.cpp
+++ LeetCode/problems/trapping_rain_water/solution.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     int trap(vector<int>& height) 
     {
-        int currTrap, totTrap = 0;
-        int n = height.size();
-        int maxLeft[n]; int maxRight[n];
-        maxLeft[0] = 0;
-        maxRight[n-1] = 0;
+       int temp, ans=0;
+       int n=height.size();
+       int maxLeft[n]; int maxRight[n];
+       maxLeft[0]=0;
+       maxRight[n-1]=0;
         
-       for(int i=1; i<n; ++i){
-           maxLeft[i] = max(maxLeft[i-1], height[i-1]);
-           maxRight[n-1-i] = max(maxRight[n-i], height[n-i]);
+       for(int i=1;i<n;++i)
+       {
+           maxLeft[i]=max(maxLeft[i-1], height[i-1]);
+           maxRight[n-1-i]=max(maxRight[n-i], height[n-i]);
        } 
-       for(int i=0; i<n; ++i){
-           currTrap = min(maxLeft[i], maxRight[i]) - height[i];
-           if(currTrap > 0)
-               totTrap += currTrap; 
+        
+       for(int i=0;i<n;++i)
+       {
+           temp=min(maxLeft[i], maxRight[i])-height[i];
+           
+           if(temp>0)
+               ans+=temp; 
        }
-       return totTrap;
+       return ans;
     }
 };
