c2ce58b2fe213e3ff386f3b053de609a963d54d9
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-10-01 06:47:53 +0000

    Sync LeetCode submission - Decode Ways (cpp)

--- LeetCode/problems/decode_ways/solution.cpp
+++ LeetCode/problems/decode_ways/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int numDecodings(string s) {
+    int numDecodings(string s) 
+    {
         
-        int n = s.length() ; 
-        vector<int> dp(n+1 , 0 ) ; 
+        int n=s.length(); 
+        vector<int> dp(n+1,0) ; 
         
-        dp[0] = 1 ;
-        if(s[0] != '0' ){
-            dp[1] = 1 ; 
-        }
-        for(int i=2 ; i<=n ; i++){
+        dp[0]=1;
+        if(s[0]!='0')
+            dp[1]=1; 
+        
+        for(int i=2;i<=n;i++)
+        {
             
-            if(s[i-1] != '0')
-                dp[i]  = dp[i-1] ; 
+            if(s[i-1]!='0')
+                dp[i]=dp[i-1] ; 
             
-            int num = (s[i-2]-'0')*10 + s[i-1]-'0' ; 
-            if(num >= 10 && num <=26)
-                dp[i] += dp[i-2] ; 
+            int num=(s[i-2]-'0')*10 + s[i-1]-'0' ;
+            
+            if(num>=10 && num<=26)
+                dp[i]+=dp[i-2] ; 
             
         }
         return dp[n] ; 
