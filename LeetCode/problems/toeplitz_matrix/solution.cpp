d8d0438a40223922ece93b1327350f82e6418ecd
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-11-03 06:57:37 +0000

    Sync LeetCode submission - Toeplitz Matrix (cpp)

---
+++ LeetCode/problems/toeplitz_matrix/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) 
+    {
+      int row=matrix.size();
+      int col=matrix[0].size();
+        
+      for(int i=1;i<row;i++)
+        for(int j=1;j<col;j++)
+          if(matrix[i-1][j-1]!=matrix[i][j])
+              return false;
+        
+      return true;
+        
+    }
+};
