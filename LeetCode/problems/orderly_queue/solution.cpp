71dc54ad102c058027f428442b8cdd134b358458
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2022-11-06 07:33:08 +0000

    Sync LeetCode submission - Orderly Queue (cpp)

--- LeetCode/problems/orderly_queue/solution.cpp
+++ LeetCode/problems/orderly_queue/solution.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string orderlyQueue(string s, int k) 
     {
-       
         if(k==1)
         {
             string ans=s;
@@ -10,6 +9,7 @@ public:
                 ans=min(ans,s.substr(i)+s.substr(0,i));
             return ans;
         }
+        
         else
         {
             sort(s.begin(),s.end());
