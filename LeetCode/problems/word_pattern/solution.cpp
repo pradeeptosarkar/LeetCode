a1c6b11bb5403b62c7708b4d28f6cbfa8d6760f8
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-01-01 14:43:26 +0000

    Sync LeetCode submission - Word Pattern (cpp)

--- LeetCode/problems/word_pattern/solution.cpp
+++ LeetCode/problems/word_pattern/solution.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
-    bool wordPattern(string pattern, string s) {
-        unordered_map<char, int> p2i;
-        unordered_map<string, int> w2i;
+    bool wordPattern(string pattern, string s) 
+    {
+        map<char, string>chToWordMap;
+        map<string, char>wordToChMap;
+        s.push_back(' ');
+        string currWord = "";
+        int chIdx = 0;
         
-        istringstream in(s); string word;
-        int i = 0, n = pattern.size();
-
-        
-        for(word; in>>word; i++){
-            if(i==n || p2i[pattern[i]] != w2i[word]) return false; //If it reaches end before all the words in string 's' are traversed || if values of keys : pattern[i] & word don't match return false
-            
-            p2i[pattern[i]] = w2i[word] = i+1; //Otherwise map to both to a value i+1
+        for (int  i = 0; i < s.size(); i++)
+        {
+            if(s[i] == ' ')
+            {
+                char currCh = pattern[chIdx++];
+                
+                if (chToWordMap.count(currCh))
+                {
+                    string alreadyMappedWord = chToWordMap[currCh];
+                    if (alreadyMappedWord != currWord) return false; 
+                }
+                else if (wordToChMap.count(currWord))
+                {
+                    char alreadyMappedCh = wordToChMap[currWord];
+                    if (alreadyMappedCh != currCh) return false; 
+                }
+                else
+                {
+                    chToWordMap[currCh] = currWord;
+                    wordToChMap[currWord] = currCh;
+                }
+                
+                currWord = "";
+            }
+            else currWord.push_back(s[i]);
         }
-        return i==n; //both the lengths should be equal
+        
+        return (chIdx == pattern.size());
     }
 };
