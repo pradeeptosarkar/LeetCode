9dc41d71f907c737bf7ab534b27b24c099d80129
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-01-31 17:30:20 +0000

    Sync LeetCode submission - Longest Arithmetic Subsequence of Given Difference (cpp)

---
+++ LeetCode/problems/longest_arithmetic_subsequence_of_given_difference/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int difference) 
+    {
+        int ans=1;
+        unordered_map<int,int> mp;
+        
+        for(auto i:arr)
+        {
+            mp[i]=mp[i-difference]+1;
+            ans=max(ans, mp[i]);
+        }
+        return ans;
+    }
+};
