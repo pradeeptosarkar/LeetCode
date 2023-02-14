208456459422b3823ec757a44dbcd636035aa2ea
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-02-14 02:20:27 +0000

    Sync LeetCode submission - Add Binary (cpp)

--- LeetCode/problems/add_binary/solution.cpp
+++ LeetCode/problems/add_binary/solution.cpp
@@ -2,36 +2,29 @@ class Solution {
 
 public:
 
-     string addBinary(string a, string b) {
-
-        //making 'a' as larger by swaping
-
-        if(a.size() < b.size()) swap(a, b); 
+     string addBinary(string a, string b) 
+     {
+        if(a.size() < b.size()) 
+            swap(a, b); 
 
         int i = a.size(), j = b.size();
 
-        //Traversing the whole "a"
+        while (i--) 
+        {
 
-        while (i--) {
+            if(j) 
+                a[i] += b[--j] & 1;
 
-            if(j) a[i] += b[--j] & 1;
-
-            if(a[i] > '1') {
-
+            if(a[i] > '1') 
+            {
                 a[i] -= 2;
 
-                if(i) 
-
+                if(i)
                     a[i-1]++; else a = '1' + a;
-
             }
-
         }
-
         return a;
-
     }
-
 };
         
 
