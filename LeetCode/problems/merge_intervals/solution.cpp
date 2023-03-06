43ca89d8f0185022363bf108608ddc0b817ef425
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-03-06 07:12:31 +0000

    Sync LeetCode submission - Merge Intervals (cpp)

--- LeetCode/problems/merge_intervals/solution.cpp
+++ LeetCode/problems/merge_intervals/solution.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& I) {
-        sort(I.begin(), I.end());
-        vector<vector<int>> ans {I[0]};                        // adding 1st interval to avoid checking ans.empty() each time in loop
-        for(auto i : I)
-            if(i[0] <= ans.back()[1])                          // merge intervals if they overlap
-                ans.back()[1] = max(ans.back()[1], i[1]);
-            else                                               // else insert new interval
-                ans.push_back(i);
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        
+        for(int i=0;i<intervals.size();i++)
+        {
+            if(ans.empty() or ans.back()[1]<intervals[i][0])
+            {
+                vector<int> temp;
+                temp.push_back(intervals[i][0]);
+                temp.push_back(intervals[i][1]);
+                ans.push_back(temp);
+            }
+            
+            else
+            {
+                ans.back()[1]=max(ans.back()[1], intervals[i][1]);
+            }
+        }
         return ans;
     }
 };
