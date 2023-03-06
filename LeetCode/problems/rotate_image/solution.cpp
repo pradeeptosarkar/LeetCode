1632c9a31d09d49d93d1c0333c6c46f4f809276f
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-03-06 06:38:45 +0000

    Sync LeetCode submission - Rotate Image (cpp)

--- LeetCode/problems/rotate_image/solution.cpp
+++ LeetCode/problems/rotate_image/solution.cpp
@@ -2,17 +2,10 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) 
     {
-        int a=0, b=matrix.size()-1;
-        
-        while(a<b)
-        {
-            for(int i=0;i<(b-a);i++)
-            {
-                swap(matrix[a][a+i], matrix[a+i][b]);
-                swap(matrix[a][a+i], matrix[b][b-i]);
-                swap(matrix[a][a+i], matrix[b-i][a]);
-            }
-            ++a, --b;
-        }
+        for(int i=0;i<matrix.size();i++)
+            for(int j=i+1;j<matrix.size();j++)
+                swap(matrix[i][j], matrix[j][i]);
+        for(int i=0;i<matrix.size();i++)
+            reverse(matrix[i].begin(), matrix[i].end());        
     }
 };
