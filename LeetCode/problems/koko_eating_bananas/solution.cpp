c136a9682b0ddb960a7d22ea9bd6481104f7ac25
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-03-08 07:17:02 +0000

    Sync LeetCode submission - Koko Eating Bananas (cpp)

--- LeetCode/problems/koko_eating_bananas/solution.cpp
+++ LeetCode/problems/koko_eating_bananas/solution.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    
-    bool is_possible(vector<int>arr,int h,int mid)
-    {     
-        int count=0;//intialise no of hour
-        int n=arr.size();
-        for(int i=0;i<n;i++)
-        {
-         count+=ceil((arr[i]*1.0)/mid);//checking time taken to eat every pile of banana
-        }
-        if(count<=h)
-        {
-            return 1;
-        }
-        return 0;
-    }
-    
-    int minEatingSpeed(vector<int>& piles, int h) {
+    int minEatingSpeed(vector<int>& piles, int h) 
+    {
+        if(h==piles.size())
+            return *max_element(piles.begin(), piles.end());
         
-        int n=piles.size();
-        int start=1;//minimum possible  banana
-        int end=*max_element(piles.begin(),piles.end());//maximum possible banana eaten
-        if(h==n)//corner case for easy computaion
-        {
-            return end;        
-        }
+        int left=0, right=INT_MAX;
         
-        int ans=-1;//intialise final answer
-        while(start<=end)
+        while(left+1<right)
         {
-            int mid=(start+end)/2;// finding middle element
-            cout<<mid<<endl;
-            if(is_possible(piles,h,mid))//checking is it possible to eat all banana if monkey eat mid banana per hour..
+            int m=(left+right)/2;
+            int temp=0;
+            
+            for(int i=0;i<piles.size();i++)
             {
-               ans=mid;//updating
-                end=mid-1;    //moving left to find more optimal answer      
+                temp+=(piles[i]/m + (piles[i]%m!=0));
+                if(temp>h)
+                    break;
             }
+            if(temp<=h)
+                right=m;
             else
-            {   
-               start=mid+1;//moving right to remove impossible cases.
-            }
+                left=m;
         }
-        return ans;
-        
+        return right;
     }
 };
