a637069884d5b4d4180fa5b93784c8d717318092
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-03-10 09:58:44 +0000

    Sync LeetCode submission - Linked List Random Node (cpp)

--- LeetCode/problems/linked_list_random_node/solution.cpp
+++ LeetCode/problems/linked_list_random_node/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    //Note : head is guaranteed to be not null, so it contains at least one node.
     ListNode* HeadNode;
-    Solution(ListNode* head) {
+    Solution(ListNode* head) 
+    {
        HeadNode = head;
     }
-    //returns value of a random node
-    int getRandom() {
+    
+    int getRandom() 
+    {
         int res, len = 1;
         ListNode* x = HeadNode;
-        while(x){
-            if(rand() % len == 0){
+        while(x)
+        {
+            if(rand() % len == 0)
                 res = x->val;
-            }
+            
             len++;
             x = x->next;
         }
