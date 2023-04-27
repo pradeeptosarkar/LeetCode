3225032ef24dfdd23b6bcc1d28a0aa558d8fdfdb
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-04-27 08:43:30 +0000

    Sync LeetCode submission - Bulb Switcher (cpp)

--- LeetCode/problems/bulb_switcher/solution.cpp
+++ LeetCode/problems/bulb_switcher/solution.cpp
@@ -2,16 +2,6 @@ class Solution {
 public:
     int bulbSwitch(int n) 
     {
-        if (n == 0) 
-            return 0;
-        long long it = 1;
-        long long counter = 1;
-        long long additioner = 3;
-        while (it <= n) {
-            it += additioner;
-            additioner += 2;
-            if (it <= n) counter++;
-        }
-        return counter; 
+        return sqrt(n);
     }
 };
