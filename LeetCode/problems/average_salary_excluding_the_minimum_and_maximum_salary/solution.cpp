d5ad8eda6e14153ee534693b1609939004e5ff8c
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-05-01 16:37:13 +0000

    Sync LeetCode submission - Average Salary Excluding the Minimum and Maximum Salary (cpp)

--- LeetCode/problems/average_salary_excluding_the_minimum_and_maximum_salary/solution.cpp
+++ LeetCode/problems/average_salary_excluding_the_minimum_and_maximum_salary/solution.cpp
@@ -1,13 +1,17 @@
-//::Pradeepto Sarkar LC1491:://
-
 class Solution {
 public:
-    double average(vector<int>& salary) {
-        sort(salary.begin(), salary.end());
-        int n=salary.size();
-        double sum=0;
-        for(int i=1;i<n-1;i++)
-            sum+=salary[i];
-        return double(sum/(n-2));
+    double average(vector<int>& salary) 
+    {
+        int maxi=999, mini=100001;
+        int sum=0,n=salary.size();
+        for(int i:salary)
+        {
+            sum+=i;
+            if(i<mini)
+                mini=i;
+            if(i>maxi)
+                maxi=i;
+        }
+        return (double)(sum-mini-maxi) / (double)(n-2);       
     }
 };
