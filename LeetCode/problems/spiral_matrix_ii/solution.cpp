382276fd6032d71c8b5fafdff5e313145a5c9b12
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-05-12 05:31:13 +0000

    Sync LeetCode submission - Spiral Matrix II (cpp)

--- LeetCode/problems/spiral_matrix_ii/solution.cpp
+++ LeetCode/problems/spiral_matrix_ii/solution.cpp
@@ -2,41 +2,24 @@ class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) 
     {
-        vector<vector<int>> ans(n,vector<int>(n,0));
-        int num = 1,r1 =0,r2 = n-1,c1 =0,c2 = n-1;
-        while(r1<=r2 && c1<=c2)
+        vector<vector<int>> ans(n, vector<int>(n));
+        int cnt=1;
+        
+        for(int layer=0; layer<(n+1)/2; layer++)
         {
-            //For first row
-            for(int i = c1;i<=c2;i++)
-            {
-                ans[r1][i] = num++;
-            }
-            //For last column
-            for(int j =r1+1;j<=r2;j++)
-            {
-                ans[j][c2] = num++;
-            }
-            //For last row
-            if(r1 != r2)
-            {
-                for(int i = c2-1;i>=c1;i--)
-                {
-                    ans[r2][i] = num++;
-                }
-            }
-            //For first column
-            if(c1 != c2)
-            {
-                for(int i = r2-1;i>r1;i--)
-                {
-                    ans[i][c1] = num++;
-                }
-            }
-            r1++;
-            r2--;
-            c1++;
-            c2--;
+            for(int ptr=layer;ptr<n-layer;ptr++)
+                ans[layer][ptr]=cnt++;
+            
+            for(int ptr=layer+1;ptr<n-layer;ptr++)
+                ans[ptr][n-layer-1]=cnt++;
+            
+            for(int ptr=n-layer-2;ptr>=layer;ptr--)
+                ans[n-layer-1][ptr]=cnt++;
+            
+            for(int ptr=n-layer-2;ptr>layer;ptr--)
+                ans[ptr][layer]=cnt++;
         }
+        
         return ans;
     }
 };
