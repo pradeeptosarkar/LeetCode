08a12b8a8d0caa959ab62d9f417b5aa3aa158448
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-05-27 14:34:52 +0000

    Sync LeetCode submission - Buy Two Chocolates (cpp)

---
+++ LeetCode/problems/buy_two_chocolates/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) 
+    {
+        sort(prices.begin(), prices.end());
+        
+        if(money-(prices[0]+prices[1])>=0)
+            return money-(prices[0]+prices[1]);
+        
+        else
+            return money;
+    }
+};
