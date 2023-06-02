772079bf0ffc3722fa42837b5bd85fe2ffe04972
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-06-02 04:41:23 +0000

    Sync LeetCode submission - Detonate the Maximum Bombs (cpp)

--- LeetCode/problems/detonate_the_maximum_bombs/solution.cpp
+++ LeetCode/problems/detonate_the_maximum_bombs/solution.cpp
@@ -1,51 +1,48 @@
-#define ll long long
 class Solution {
 public:
-    bool isInside(ll circle_x, ll circle_y, ll rad, ll x, ll y){
-        if ((x - circle_x) * (x - circle_x) + (y - circle_y) * (y - circle_y) <= rad * rad) return true;
-        else return false;
-    }
-    
-    int bfs(int i, unordered_map<int,vector<int>> &map, int n){
-        vector<int> vis(n,false);
-        queue<int> q;
-        q.push(i);
-        vis[i] = true;
-        int count=0;
-        while(!q.empty()){
-            int cur = q.front(); q.pop();
-            count++;
-            for(auto &child : map[cur]){
-                if(vis[child]==false){
-                    q.push(child);
-                    vis[child] = true;
-                } 
-            }
+    int dfs(int node, unordered_map<int, vector<int> > &mp, vector<bool> &vis)
+    {
+        if(vis[node]){
+           return 0;
         }
-        return count;
+        vis[node]=true;
+        int num=1;
+        for(auto it: mp[node])
+        {
+            num +=dfs(it,mp,vis);
+        }
+        return num;
     }
-    
     int maximumDetonation(vector<vector<int>>& bombs) {
-        int n = bombs.size();
-        unordered_map<int,vector<int>> map;
-        
-        for(int i=0; i<n; ++i){
-            int circle_x = bombs[i][0];
-            int circle_y = bombs[i][1];
-            int rad = bombs[i][2];
-            for(int j=0; j<n; ++j){
-                if(i!=j){
-                    int x = bombs[j][0];
-                    int y = bombs[j][1];
-                    if(isInside(circle_x, circle_y, rad, x, y)) map[i].push_back(j);
-                } 
+        unordered_map<int, vector<int> > mp;
+        int n=bombs.size();
+        for(int i=0;i<n;i++)
+        {
+            long long  x1,y1,r1;
+            x1=bombs[i][0];
+            y1=bombs[i][1];
+            r1=bombs[i][2];
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j)
+                {
+                     long long x2,y2,r2;
+                     x2=abs(x1-bombs[j][0]);
+                     y2=abs(y1-bombs[j][1]);
+                    if(x2*x2+y2*y2<=r1*r1)
+                    {
+                        mp[i].push_back(j);
+                    }
+                }
             }
         }
-        
-        int maxDefuse = 0;
-        for(int i=0; i<n; ++i){
-            maxDefuse = max(maxDefuse, bfs(i,map,n));
+       
+        int maxi=-1;
+        for(int i=0;i<bombs.size();i++)
+        {
+            vector<bool> vis(bombs.size(),false);
+          maxi=max(maxi,dfs(i,mp,vis));
         }
-        return maxDefuse;
+        return maxi;
     }
 };
