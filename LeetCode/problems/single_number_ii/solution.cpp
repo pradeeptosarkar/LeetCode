fa79ddb86c47ed6c6ad58769b0f78891da1e98a6
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-07-04 03:40:28 +0000

    Sync LeetCode submission - Single Number II (cpp)

--- LeetCode/problems/single_number_ii/solution.cpp
+++ LeetCode/problems/single_number_ii/solution.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) 
     {
-        map<int,int> count;
+        int ans=0;
         
-        for(int i:nums)
-            count[i]++;
+        for(int i=0;i<32;i++)
+        {
+            int setCount=0;
+            
+            for(int j=0;j<nums.size();j++)
+                setCount+=((nums[j]>>i)&1);
+            
+            setCount%=3;
+            ans|=setCount<<i;
+        }
         
-        for(int i:nums){
-            if(count[i]==1)
-                return i;}
-        return -1;
+        return ans;
     }
 };
