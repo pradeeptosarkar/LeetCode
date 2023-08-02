e724f9097eaf00929259b3f6eb3a9dbe7ce0975e
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-08-02 17:15:56 +0000

    Sync LeetCode submission - Majority Element II (cpp)

--- LeetCode/problems/majority_element_ii/solution.cpp
+++ LeetCode/problems/majority_element_ii/solution.cpp
@@ -2,23 +2,41 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) 
     {
-        map<int,int> count;
+        int temp1=INT_MIN, temp2=INT_MIN;
+        int cnt1=0, cnt2=0;
         
-        for(auto num: nums)
-            count[num]++;
-        
-        int times=nums.size()/3;
-        
-        vector<int> res;
-        
-        for(auto it: count)
+        for(int i=0;i<nums.size();i++)
         {
-            if(it.second>times)
-                res.push_back(it.first);
+            if(cnt1==0 and temp2!=nums[i])
+                temp1=nums[i], cnt1+=1;
             
-            if(res.size()==2)
-                break;
+            else if(cnt2==0 and temp1!=nums[i])
+                temp2=nums[i], cnt2+=1;
+            
+            else if(temp1==nums[i])
+                cnt1+=1;
+            
+            else if(temp2==nums[i])
+                cnt2+=1;
+            
+            else
+                cnt1-=1, cnt2-=1;            
         }
-        return res;
+        
+        vector<int> ans;
+        cnt1=0, cnt2=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(temp1==nums[i]) cnt1+=1;
+            if(temp2==nums[i]) cnt2+=1;
+        }
+        
+        int check = (int)nums.size()/3;
+        
+        if(cnt1>check) ans.push_back(temp1);
+        if(cnt2>check) ans.push_back(temp2);
+        
+        return ans;
     }
 };
