008ec0324df770b0c10381a9126a233f11b640f7
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-08-28 06:17:20 +0000

    Sync LeetCode submission - Counter (javascript)

---
+++ LeetCode/problems/counter/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    //let count=n-1;
+    return function() {
+        //return count+=1;
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
