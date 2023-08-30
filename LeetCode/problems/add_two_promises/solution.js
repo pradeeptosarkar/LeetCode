c808993f9547031b29e525719b94b964a4598fbc
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-08-30 06:10:51 +0000

    Sync LeetCode submission - Add Two Promises (javascript)

---
+++ LeetCode/problems/add_two_promises/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) 
+{
+    return Promise.all([promise1, promise2]).then((vals) => {
+        return vals[0] + vals[1];
+    })
+    
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
