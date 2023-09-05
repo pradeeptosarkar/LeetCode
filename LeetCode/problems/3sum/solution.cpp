f8bd3f7e6afba856ce7b7433fc7cc2a71cd9feae
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-09-05 07:41:26 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- LeetCode/problems/3sum/solution.cpp
+++ LeetCode/problems/3sum/solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
         vector<vector<int>> ans;
         int n = nums.size();
         sort(nums.begin(), nums.end());
         
-        for (int i = 0; i < n - 2; ++i) {
-            // Avoid duplicates for the first element
+        for (int i = 0; i < n - 2; ++i) 
+        {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             
@@ -14,24 +15,27 @@ public:
             int left = i + 1;
             int right = n - 1;
             
-            while (left < right) {
+            while (left < right) 
+            {
                 int sum = nums[left] + nums[right];
                 
                 if (sum == target) {
                     ans.push_back({nums[i], nums[left], nums[right]});
-                    // Avoid duplicates for the second element
+                    
                     while (left < right && nums[left] == nums[left + 1])
                         left++;
-                    // Avoid duplicates for the third element
+                    
                     while (left < right && nums[right] == nums[right - 1])
                         right--;
                     left++;
                     right--;
-                } else if (sum < target) {
+                } 
+                
+                else if (sum < target) 
                     left++;
-                } else {
+                
+                else 
                     right--;
-                }
             }
         }
         
