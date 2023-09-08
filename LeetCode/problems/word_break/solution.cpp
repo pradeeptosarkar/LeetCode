86b7aaa4d9e58368be19df27613edd5f0eee2054
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-09-08 07:51:10 +0000

    Sync LeetCode submission - Word Break (cpp)

--- LeetCode/problems/word_break/solution.cpp
+++ LeetCode/problems/word_break/solution.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
-    vector<int>poss;
-    bool checker(string &s, int st, int en, vector<string>&wordDict) {
-        if (st == en)return true;
-        bool val = false;
-        for (int i = 1; i + st - 1 < en; i++) {
-            string k = s.substr(st, i);
-            if (find(wordDict.begin(), wordDict.end(), k) != wordDict.end()) {
-                // cout << k << endl;
-                // cout << st << " " << en << endl;
-                if(poss[st+i-1] == -1){
-                   val |= checker(s, st + i, en, wordDict);
-                   poss[st+i-1] = val;
-                }else{
-                    val |= poss[st+i-1];
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // Create a set for faster word lookup
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        // Create a memoization table to store intermediate results
+        unordered_map<string, bool> memo;
+        return canSegment(s, wordSet, memo);
+    }
+
+    bool canSegment(string s, unordered_set<string>& wordSet, unordered_map<string, bool>& memo) {
+        if (s.empty()) {
+            return true; // We've successfully segmented the string
+        }
+
+        // Check if the result for the current string is already memoized
+        if (memo.find(s) != memo.end()) {
+            return memo[s];
+        }
+
+        // Try to find a word from the dictionary that can be used as a prefix of s
+        for (int i = 1; i <= s.length(); i++) {
+            string prefix = s.substr(0, i);
+            if (wordSet.find(prefix) != wordSet.end()) {
+                // If we found a valid word, recursively check the remaining part of the string
+                string remaining = s.substr(i);
+                if (canSegment(remaining, wordSet, memo)) {
+                    memo[s] = true;
+                    return true;
                 }
             }
         }
-        return val;
-    }
-    bool wordBreak(string s, vector<string>& wordDict) {
-        poss.resize(s.length(), -1);
-        int st = 0, en = s.length();
-        return checker(s, st, en, wordDict);
+
+        // If no valid segmentation is found, memoize and return false
+        memo[s] = false;
+        return false;
     }
 };
