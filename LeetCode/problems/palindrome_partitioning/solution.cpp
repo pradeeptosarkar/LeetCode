5210dd254930bf6ac8bbc3e1fc8ea394e86ceabb
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-09-12 07:20:58 +0000

    Sync LeetCode submission - Palindrome Partitioning (cpp)

--- LeetCode/problems/palindrome_partitioning/solution.cpp
+++ LeetCode/problems/palindrome_partitioning/solution.cpp
@@ -1,36 +1,41 @@
-//::Pradeepto Sarkar LC131:://
-
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> path;
-        partition(s, 0, path, result);//dfs calls
-        return result;
-    }
-private: 
-    //DFS steps
-    void partition(string& s, int start, vector<string>& path, vector<vector<string>>& result) {
-        int n = s.length();
-        if (start == n) {
-            result.push_back(path);
-        } else {
-            for (int i = start; i < n; i++) {
-                if (isPalindrome(s, start, i)) {
-                    path.push_back(s.substr(start, i - start + 1));
-                    partition(s, i + 1, path, result);
-                    path.pop_back();
-                }
-            }
-        }
-    }
-    //helper function to safe check whether a substr is palindrome or not
-    bool isPalindrome(string& s, int l, int r) {
-        while (l < r) {
-            if (s[l++] != s[r--]) {
+    
+    bool checkPalindrome(string& s, int start, int end)
+    {
+        while(start<=end)
+        {
+            if(s[start++]!=s[end--])
                 return false;
+        }
+            
+        return true;
+    }
+    
+    void solve(string& s, vector<vector<string>>& ans, vector<string>& temp, int index)
+    {
+        if(index==s.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        for(int i=index; i<s.size(); i++)
+        {
+            if(checkPalindrome(s, index, i))
+            {
+                temp.push_back(s.substr(index, i-index+1));
+                solve(s, ans, temp, i+1);
+                temp.pop_back();
             }
         }
-        return true;
+    }
+    
+    vector<vector<string>> partition(string s) 
+    {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(s, ans, temp, 0);
+        return ans;        
     }
 };
