3da5c68fddeb3b100cc5b42d9e7b31a28a3372af
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-09-17 08:47:07 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (cpp)

--- LeetCode/problems/search_in_rotated_sorted_array/solution.cpp
+++ LeetCode/problems/search_in_rotated_sorted_array/solution.cpp
@@ -1,23 +1,34 @@
-//:: Pradeepto Sarkar LC33 26/02/2022 :://
-
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l,r,m,k=0;
-        l=1,r=nums.size()-1;
-        while(l<=r)
+    int search(vector<int>& nums, int target) 
+    {
+        int left=0, right=nums.size()-1;
+        int mid;
+        
+        while(left<=right)
         {
-            m=(l+r)/2;
-            if(nums[m]<nums[0]) 
-                k=m,r=m-1;
-            else l=m+1;
+            mid=(left+right)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+            
+            if(nums[left]<=nums[mid])
+            {
+                if(nums[left]<=target and target<=nums[mid])
+                    right=mid-1;
+                
+                else
+                    left=mid+1;
+            }
+            
+            else
+            {
+                if(nums[mid]<=target and target<=nums[right])
+                    left=mid+1;
+                else
+                    right=mid-1;
+            }
         }
-        
-        if(binary_search(nums.begin(),nums.begin()+k,target))
-            return (lower_bound(nums.begin(),nums.begin()+k,target)-nums.begin());
-        
-        if(binary_search(nums.begin()+k,nums.end(),target))
-            return (lower_bound(nums.begin()+k,nums.end(),target)-(nums.begin()));
         
         return -1;
     }
