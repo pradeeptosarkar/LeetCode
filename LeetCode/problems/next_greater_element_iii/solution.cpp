0e865f69efb5df8ab07f98c1c47b9f91812f4c22
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-09-23 11:42:50 +0000

    Sync LeetCode submission - Next Greater Element III (cpp)

---
+++ LeetCode/problems/next_greater_element_iii/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+int nextGreaterElement(int n) 
+{
+    auto digits = to_string(n);
+    next_permutation(begin(digits), end(digits));
+    auto res = stoll(digits);
+    return (res > INT_MAX || res <= n) ? -1 : res;
+}
+};
