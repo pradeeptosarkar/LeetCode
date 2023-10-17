5fb2dc6ffcefa80031f4da67547e11f4a76805d2
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-10-17 12:38:03 +0000

    Sync LeetCode submission - String to Integer (atoi) (cpp)

--- LeetCode/problems/string_to_integer_(atoi)/solution.cpp
+++ LeetCode/problems/string_to_integer_(atoi)/solution.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-  int myAtoi(string s) {
+  int myAtoi(string s) 
+  {
         long l=s.length(),num=0;
         int i=0,j=1;
+      
         for(i=0;i<l;i++)
-        {
             if(s[i]!=' ')break;
+        
+        if(s[i]=='-')
+        {
+            j=-1;i++;
         }
-        if(s[i]=='-'){j=-1;i++;}
-        else if(s[i]=='+')i++;
+        else if(s[i]=='+')
+            i++;
+      
         for(;i<l;i++)
         {
             if(s[i]>='0'&&s[i]<='9')  
@@ -18,23 +24,24 @@ public:
         {
             return INT32_MIN;
         }
+                
         if(j*num>INT32_MAX)
-        {
             return INT32_MAX;
-        }
-            }else{
-                break;
+        
             }
+            
+            else
+                break;
+            
         }
+      
         num*=j;
         if(num<INT32_MIN)
-        {
             return INT32_MIN;
-        }
+      
         if(num>INT32_MAX)
-        {
             return INT32_MAX;
-        }
+        
         return num;
     }
 };
