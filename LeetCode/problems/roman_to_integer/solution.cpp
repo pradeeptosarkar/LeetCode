23aa320e9964a1fafca5887f2f7c7febb5f33e39
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-10-17 12:31:53 +0000

    Sync LeetCode submission - Roman to Integer (cpp)

--- LeetCode/problems/roman_to_integer/solution.cpp
+++ LeetCode/problems/roman_to_integer/solution.cpp
@@ -11,14 +11,19 @@ public:
                                    { 'M' , 1000 } };
                                    
    int sum = T[s.back()];
-   for (int i=s.length()-2; i>=0; --i) 
+   for (int i = s.length() - 2; i >= 0; --i) 
    {
        if (T[s[i]] < T[s[i + 1]])
+       {
            sum -= T[s[i]];
-
+       }
        else
+       {
            sum += T[s[i]];
+       }
    }
+    
+   
    return sum;
 }
 };
