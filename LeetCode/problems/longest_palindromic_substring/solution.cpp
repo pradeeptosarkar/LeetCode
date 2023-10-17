0e03bce8776558b34a500b695fa102cba43e1c15
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-10-17 12:26:15 +0000

    Sync LeetCode submission - Longest Palindromic Substring (cpp)

--- LeetCode/problems/longest_palindromic_substring/solution.cpp
+++ LeetCode/problems/longest_palindromic_substring/solution.cpp
@@ -1,38 +1,39 @@
 class Solution {
+private: 
+    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
+        if(i == j){
+            return dp[i][j] = true;
+        }
+        if(j-i == 1){
+            if(s[i] == s[j]){
+                return dp[i][j] = true;
+            }
+            else{
+                return dp[i][j] = false;
+            }
+        }
+        if(s[i] == s[j] && dp[i+1][j-1] == true){
+            return dp[i][j] = true;
+        } else {
+            return dp[i][j] = false;
+        }
+    }
 public:
     string longestPalindrome(string s) {
-        const int len = 2*s.length()+1;
-        int P[len];
-        int center=0, right=0, maxlen=0;
-        string ret="";
-        for (int i=0; i<len; i++) {
-            P[i] = 0;
-            if (i<right) {
-                P[i] = std::min(right-i, P[2*center-i]);
-            }
-            int a=i-P[i]-1, b=i+P[i]+1;
-            while (a>-1 and b<len) {
-                if (a%2==0 or s[a/2]==s[b/2]) {
-                    a--;
-                    b++;
-                    P[i]++;
-                } else {
-                    break;
-                }
-            }
-            if (i+P[i]>right) {
-                right = i+P[i];
-                center = i;
-                if (maxlen<P[i]) {
-                    maxlen=P[i];
-                    if ((i-P[i])%2 == 0) {
-                        ret = s.substr((i-P[i])/2, (i+P[i])/2-(i-P[i])/2);
-                    } else {
-                        ret = s.substr((i-1-P[i])/2, (i-1+P[i])/2-(i-1-P[i])/2);
+        int n = s.size();
+        int startIndex = 0; int maxlen = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int g=0; g<n; g++){
+            for(int i=0, j=g; j<n; i++, j++){
+                solve(dp, i, j, s);
+                if(dp[i][j] == true){
+                    if(j-i+1 > maxlen){
+                        startIndex = i;
+                        maxlen = j-i+1;
                     }
                 }
             }
         }
-        return ret;
+        return s.substr(startIndex, maxlen);
     }
 };
