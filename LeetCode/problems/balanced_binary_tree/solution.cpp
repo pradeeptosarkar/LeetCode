14c1c14d8fbebe32f8503cb7e777f77733b566f0
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-12-04 15:56:26 +0000

    Sync LeetCode submission - Balanced Binary Tree (cpp)

--- LeetCode/problems/balanced_binary_tree/solution.cpp
+++ LeetCode/problems/balanced_binary_tree/solution.cpp
@@ -1,20 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int height(TreeNode* root)
+    
+    int check(TreeNode* root)
     {
-        if(root==NULL)return 0;
-
-        int l=height(root->left);
-        int r=height(root->right);
+        if(root==NULL)
+            return 0;
         
-        if(l==-1 or r==-1 or abs(l-r)>=2)
+        int l=check(root->left);
+        int r=check(root->right);
+        
+        if(l==-1 or r==-1)
             return -1;
         
-        return 1+max(l,r);
+        if(abs(l-r)>1)
+            return -1;
+        
+        return max(l,r)+1;
     }
     
     bool isBalanced(TreeNode* root) 
     {
-        return height(root)!=-1;
+        return check(root)!=-1;        
     }
 };
