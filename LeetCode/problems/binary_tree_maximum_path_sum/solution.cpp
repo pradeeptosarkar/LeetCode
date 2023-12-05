d5fc9aa1f57b34270d4c0c1b9fdc814166760ef3
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-12-05 07:12:01 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (cpp)

---
+++ LeetCode/problems/binary_tree_maximum_path_sum/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int solve(TreeNode* root, int& ans)
+    {
+        if(root==NULL)
+            return 0;
+        
+        int l=max(0,solve(root->left, ans));
+        int r=max(0,solve(root->right, ans));
+        
+        ans=max(ans, l+r+root->val);
+        
+        return root->val+max(l,r);
+    }
+    
+    int maxPathSum(TreeNode* root) 
+    {
+        int ans=INT_MIN;
+        solve(root,ans);
+        return ans;
+    }
+};
