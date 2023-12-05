24bc042712f66f3f7a45d01dd871a81e54b1c902
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-12-05 08:44:13 +0000

    Sync LeetCode submission - Same Tree (cpp)

--- LeetCode/problems/same_tree/solution.cpp
+++ LeetCode/problems/same_tree/solution.cpp
@@ -13,39 +13,9 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) 
     {
-        TreeNode* a=p;
-        TreeNode* b=q;
+        if(p==NULL or q==NULL)
+            return (p==q);
         
-        stack<TreeNode*> stk1({a}), stk2({b});
-        
-        while(!stk1.empty() or !stk2.empty())
-        {
-            a=stk1.top();
-            b=stk2.top();
-            
-            if(a==NULL and b==NULL)
-                ;
-            else if(a==NULL or b==NULL)
-                return false;
-            else if(a->val!=b->val)
-                return false;
-            
-            stk1.pop();
-            stk2.pop();
-            
-            if(a!=NULL)
-            {
-                stk1.push(a->left);
-                stk1.push(a->right);
-            }
-            
-            if(b!=NULL)
-            {
-                stk2.push(b->left);
-                stk2.push(b->right);
-            }
-        }
-        
-        return true;
+        return (p->val==q->val and isSameTree(p->left,q->left) and isSameTree(p->right,q->right));        
     }
 };
