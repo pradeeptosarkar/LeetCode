3913fb7ccd389b10c27440b1806040c0a792a49c
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-12-06 10:25:13 +0000

    Sync LeetCode submission - Symmetric Tree (cpp)

--- LeetCode/problems/symmetric_tree/solution.cpp
+++ LeetCode/problems/symmetric_tree/solution.cpp
@@ -1,14 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isMirror(TreeNode *r1,TreeNode *r2){
-    if(r1==NULL && r2==NULL)
-        return true;
-    if(r1 && r2 && r1->val == r2->val){
-        return isMirror(r1->left,r2->right) && isMirror(r1->right,r2->left);
+    
+    bool solve(TreeNode* left, TreeNode* right)
+    {
+        if(left==NULL or right==NULL)
+            return left==right;
+        
+        if(left->val!=right->val)
+            return false;
+        
+        return solve(left->left,right->right) and solve(left->right, right->left);
     }
-    return false;
-}
-    bool isSymmetric(TreeNode* root) {
-        return isMirror(root,root);
+    
+    bool isSymmetric(TreeNode* root) 
+    {
+        if(!root or (!root->left and !root->right))
+            return true;
+        
+        return solve(root->left, root->right);
     }
 };
