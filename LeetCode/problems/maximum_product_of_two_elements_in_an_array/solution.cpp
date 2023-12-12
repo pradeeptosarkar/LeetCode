76d3ee7d880520506015646cf25070120efbebc4
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2023-12-12 13:53:52 +0000

    Sync LeetCode submission - Maximum Product of Two Elements in an Array (cpp)

---
+++ LeetCode/problems/maximum_product_of_two_elements_in_an_array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        int biggest = 0;
+        int secondBiggest = 0;
+        
+        for (int num : nums) 
+        {
+            if (num > biggest) 
+            {
+                secondBiggest = biggest;
+                biggest = num;
+            } 
+            else {
+                secondBiggest = max(secondBiggest, num);
+            }
+        }
+        
+        return (biggest - 1) * (secondBiggest - 1);
+    }
+};
