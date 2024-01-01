4ba1aa8e8562de3d693f9251665ea88486002ba2
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-01-01 01:55:34 +0000

    Sync LeetCode submission - Maximum Width of Binary Tree (cpp)

--- LeetCode/problems/maximum_width_of_binary_tree/solution.cpp
+++ LeetCode/problems/maximum_width_of_binary_tree/solution.cpp
@@ -1,35 +1,50 @@
-class Solution 
-{
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) 
     {
-        if(root==NULL)
+        if(!root)
             return 0;
         
         int ans=0;
-        queue<pair<TreeNode*, int>>q;
-        q.push({root, 0});
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
         
         while(!q.empty())
         {
-            int levelSize=q.size();
-            int start=q.front().second;
-            int end=q.back().second;
-            
-            ans=max(ans, end-start+1);
-            
-            for(int i=0;i<levelSize;i++)
+            int size=q.size();
+            int tempMin=q.front().second;
+            int first, last;
+        
+            for(int i=0;i<size;i++)
             {
-                TreeNode* tempNode=q.front().first;
-                int nodeIndex=q.front().second;
+                long long int curr=q.front().second-tempMin;
+                TreeNode* temp=q.front().first;
                 q.pop();
                 
-                if(tempNode->left!=NULL)
-                    q.push({tempNode->left, (long long)2*nodeIndex+1});
-                if(tempNode->right!=NULL)
-                    q.push({tempNode->right, (long long)2*nodeIndex+2});  
+                if(i==0) first=curr;
+                if(i==size-1) last=curr;
+                
+                if(temp->left)
+                    q.push({temp->left, curr*2+1});
+                
+                if(temp->right)
+                    q.push({temp->right, curr*2+2});
             }
+            
+            ans=max(ans, last-first+1);
         }
+        
         return ans;
     }
-};  
+};
