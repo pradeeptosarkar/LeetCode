765806f9676d96c517cbedd3e06b4e9389b0809b
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-01-10 04:08:11 +0000

    Sync LeetCode submission - Amount of Time for Binary Tree to Be Infected (cpp)

---
+++ LeetCode/problems/amount_of_time_for_binary_tree_to_be_infected/solution.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int maxDistance = 0;
+    
+    int traverse(TreeNode* root, int start) 
+    {
+        int depth = 0;
+        
+        if (!root) 
+            return depth;
+
+        int leftDepth = traverse(root->left, start);
+        int rightDepth = traverse(root->right, start);
+
+        if (root->val == start) 
+        {
+            maxDistance = max(leftDepth, rightDepth);
+            depth = -1;
+        } 
+        
+        else if (leftDepth >= 0 && rightDepth >= 0) 
+        {
+            depth = max(leftDepth, rightDepth) + 1;
+        } 
+        
+        else 
+        {
+            int distance = abs(leftDepth) + abs(rightDepth);
+            maxDistance = max(maxDistance, distance);
+            depth = min(leftDepth, rightDepth) - 1;
+        }
+
+        return depth;
+    }
+    
+    int amountOfTime(TreeNode* root, int start) 
+    {
+        traverse(root, start);
+        return maxDistance;        
+    }
+};
