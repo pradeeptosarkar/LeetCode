7e2b2e202f025a955f86a5ce3344ea4f6d625955
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-01-12 05:37:16 +0000

    Sync LeetCode submission - Determine if String Halves Are Alike (cpp)

--- LeetCode/problems/determine_if_string_halves_are_alike/solution.cpp
+++ LeetCode/problems/determine_if_string_halves_are_alike/solution.cpp
@@ -1,46 +1,14 @@
 class Solution {
 public:
-    bool halvesAreAlike(string s) {
+    bool halvesAreAlike(string s) 
+    {
+        int ans=0;
+        int n=s.size();
         
-        string vowels = "aeiouAEIOU";
-        int n = s.length();
-        
-        int alikeOne  = 0, alikeTwo = 0;
-        
-		//cout statements are for debugging purposes on a cpp compiler
-        string halfOne = s.substr(0, (n/2));
-        //cout << (n/2) - 1<< endl;
-        //cout << halfOne << endl;
-        string halfTwo = s.substr(n/2, n-1);
-        //cout << halfTwo << endl;
-        
-        for(char ch : vowels)
-        {
-            for(char c: halfOne)
-            {
-                if(ch == c)
-                {
-                    alikeOne++;
-                }
-            }
-            for(char c: halfTwo)
-            {
-                if(ch == c)
-                {
-                    alikeTwo++;
-                }
-            }
-        }
-        
-        //cout << alikeOne << endl << alikeTwo << endl;
-        
-        if(alikeOne == alikeTwo)
-        {
-            return true;
-        }
-        
-        
-        return false;
-        
+        for(int i=0;i<n;i++)
+            if(s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A' or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U')
+                (i<n/2)?ans+=1:ans-=1;
+            
+        return !ans;
     }
 };
