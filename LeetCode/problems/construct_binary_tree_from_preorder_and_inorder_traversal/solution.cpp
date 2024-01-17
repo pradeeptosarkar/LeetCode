1c8fc1ce910cbe3e6da1ca24ddfd22e74fb1643b
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-01-17 15:26:46 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (cpp)

--- LeetCode/problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.cpp
+++ LeetCode/problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.cpp
@@ -11,30 +11,31 @@
  */
 class Solution {
 public:
-    
-    TreeNode* construct(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, map<int,int>& mp)
-    {
-        if(preStart>preEnd or inStart>inEnd)
-            return NULL;
-        
-        TreeNode* root= new TreeNode(preorder[preStart]);
-        int inRoot=mp[root->val];
-        int numsLeft=inRoot-inStart;
-        
-        root->left=construct(preorder, preStart+1, preStart+numsLeft, inorder, inStart, inRoot-1, mp);
-        root->right=construct(preorder, preStart+numsLeft+1, preEnd, inorder, inRoot+1, inEnd, mp);
-        return root;
-        
-    }
-    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) 
     {
         map<int,int> mp;
         
-        for(int i=0;i<preorder.size();i++)
+        for(int i=0;i<inorder.size();i++)
             mp[inorder[i]]=i;
         
-        return construct(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, mp);
+        TreeNode* root=buildNewTree(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mp);
         
+        return root;
+    }
+    
+    TreeNode* buildNewTree(vector<int> &preorder, int preStart, int preEnd, vector<int> &inorder, int inStart, int inEnd, map<int,int> &mp)
+    {
+        if(preStart>preEnd or inStart>inEnd)
+            return NULL;
+        
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        
+        int inRoot=mp[root->val];
+        int leftNums=inRoot-inStart;
+        
+        root->left= buildNewTree(preorder, preStart+1, preStart+leftNums, inorder, inStart, inRoot-1, mp);
+        root->right= buildNewTree(preorder, preStart+leftNums+1, preEnd, inorder, inRoot+1, inEnd, mp);
+        
+        return root;
     }
 };
