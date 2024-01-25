95b025d0cb6560000ceb5403f13942dec5544f37
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-01-25 18:07:55 +0000

    Sync LeetCode submission - Binary Search Tree Iterator (cpp)

--- LeetCode/problems/binary_search_tree_iterator/solution.cpp
+++ LeetCode/problems/binary_search_tree_iterator/solution.cpp
@@ -1,29 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class BSTIterator {
+    
+    stack<TreeNode *> myStack;
+    
 public:
-     stack<TreeNode*> st;
-    BSTIterator(TreeNode *root) {
-        Lefttree(root);
+    BSTIterator(TreeNode* root) {
+        push(root);
+    }
+    
+    int next() 
+    {
+        TreeNode* temp=myStack.top();
+        myStack.pop();
+        push(temp->right);
+        return temp->val;
     }
     
     bool hasNext() {
-    return st.empty() ? false : true;
+        return !myStack.empty();
     }
     
-    int next() {
-        TreeNode* top = st.top();
-        st.pop();
-        if (top->right != NULL)
-            Lefttree(top->right);  
-     return top->val;
-    }
-    
-    void Lefttree(TreeNode* root)
-    {
-        TreeNode* p = root;
-        while (p)
-        {
-            st.push(p);
-            p = p->left;
-        }
-    }
+    private:
+
+void push(TreeNode *node)
+{
+    for(;node!=NULL;myStack.push(node),node=node->left);
+}
 };
+
+
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
