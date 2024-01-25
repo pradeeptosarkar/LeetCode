4fe1fa40ab1977941c7d94f9305054b33133c02a
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-01-25 07:25:40 +0000

    Sync LeetCode submission - Longest Common Subsequence (cpp)

--- LeetCode/problems/longest_common_subsequence/solution.cpp
+++ LeetCode/problems/longest_common_subsequence/solution.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int text1Len = text1.size();
-        int text2Len = text2.size();
+    int longestCommonSubsequence(string text1, string text2) 
+    {
+        int n=text1.size();
+        int m=text2.size();
         
-        // i: index of text1
-        // j: index of text2
-        // dp[i][j] is the length of the longest common subsequence 
-        // of text1[0..i-1] and text2[0..j-1].
-        vector<vector<int>> dp(text1Len + 1, vector<int>(text2Len + 1, 0));
+        vector<int> prev(m+1,0), curr(m+1,0);
         
-        for (int i = 1; i <= text1Len; ++i) {
-            for (int j = 1; j <= text2Len; ++j) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
+        for(int j=0;j<=m;j++)
+            prev[j]=0;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                    curr[j]=1+prev[j-1];
+                
+                else curr[j]=max(prev[j], curr[j-1]);
             }
+            
+            prev=curr;
         }
         
-        return dp[text1Len][text2Len];
+        return prev[m];
     }
 };
