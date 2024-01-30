0560e106567216b6339efee14fce596a8aae7924
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-01-30 09:03:28 +0000

    Sync LeetCode submission - Two Sum IV - Input is a BST (cpp)

--- LeetCode/problems/two_sum_iv_-_input_is_a_bst/solution.cpp
+++ LeetCode/problems/two_sum_iv_-_input_is_a_bst/solution.cpp
@@ -9,20 +9,77 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class BSTIterator
+{
+    stack<TreeNode *> stk;
+    
+    bool reverse=true;
+    
+    public:
+    
+    BSTIterator(TreeNode *root, bool isRev)
+    {
+        reverse=isRev;
+        pushAll(root);
+    }
+    
+    bool hasNext()
+    {
+        return !stk.empty();
+    }
+    
+    int next()
+    {
+        TreeNode *temp=stk.top();
+        stk.pop();
+        
+        if(!reverse) 
+            pushAll(temp->right);
+        else
+            pushAll(temp->left);
+        
+        return temp->val;
+    }
+    
+    private:
+    
+    void pushAll(TreeNode *node)
+    {
+        for(;node!=NULL;)
+        {
+            stk.push(node);
+            if(reverse)
+                node=node->right;
+            else
+                node=node->left;
+        }
+    }
+};
+
 class Solution {
 public:
-    
-    set<int> s;
-    
     bool findTarget(TreeNode* root, int k) 
     {
-        if(root==NULL)
+        if(!root)
             return false;
-        if(s.count(k-root->val))
-            return 1;
-        s.insert(root->val);
         
-        return findTarget(root->left,k) or findTarget(root->right,k);
+        BSTIterator left(root, false);
+        BSTIterator right(root, true);
         
+        int i=left.next();
+        int j=right.next();
+        
+        while(i<j)
+        {
+            if(i+j==k)
+                return true;
+            else if(i+j<k)
+                i=left.next();
+            else
+                j=right.next();
+        }
+        
+        return false;
     }
 };
