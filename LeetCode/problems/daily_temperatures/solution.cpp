ea7686144f15eff0cfc966b19ee10c58e47a19bd
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-01-31 04:41:09 +0000

    Sync LeetCode submission - Daily Temperatures (cpp)

--- LeetCode/problems/daily_temperatures/solution.cpp
+++ LeetCode/problems/daily_temperatures/solution.cpp
@@ -3,20 +3,25 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) 
     {
         int n=temperatures.size();
-        stack<int> temp;
-        vector<int> res(n,0);
+        vector<int> ans(n,0);
+        int hottest=-1;
+        
         for(int i=n-1;i>=0;i--)
         {
-           while(!temp.empty() && temperatures[temp.top()]<=temperatures[i])
-           {
-               temp.pop();
-           }
-           if(!temp.empty())
-               res[i]=temp.top()-i;
-                     
-           temp.push(i);
+            if(temperatures[i]>=hottest)
+            {
+                hottest=temperatures[i];
+                continue;
+            }
+            
+            int days=1;
+            
+            while(temperatures[i+days]<=temperatures[i])
+                days+=ans[i+days];
+            
+            ans[i]=days;
         }
-       
-        return res;
+        
+        return ans;
     }
 };
