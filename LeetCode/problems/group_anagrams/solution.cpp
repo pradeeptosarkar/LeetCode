cbf74c888eea4afcaeaeb759e443834d371f46cd
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-02-06 06:28:10 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- LeetCode/problems/group_anagrams/solution.cpp
+++ LeetCode/problems/group_anagrams/solution.cpp
@@ -2,20 +2,69 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) 
     {
-        unordered_map<string,vector<string>> mp;
-        
-        for(string i:strs)
+        unordered_map<Key, vector<string>, Key::Hash> dict;
+
+        for (const auto& str : strs) 
         {
-            string temp=i;
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(i);
+            Key key = generateHash(str);
+
+            if (dict.find(key) != dict.end())
+                dict[key].push_back(str);
+            else
+                dict[key] = { str };
         }
-        vector<vector<string>>  ans;
+
+        vector<vector<string>> result;
         
-        for(auto i:mp)
-            ans.push_back(i.second);
+        for (const auto& kvp : dict) 
+            result.push_back(kvp.second);
         
-        return ans;
-        
+        return result;
+    }
+
+private:
+    struct Key 
+    {
+        long long first;
+        long long second;
+        long long third;
+
+        bool operator==(const Key& other) const 
+        {
+            return first == other.first && second == other.second && third == other.third;
+        }
+
+        struct Hash 
+        {
+            size_t operator()(const Key& k) const 
+            {
+                return hash<long long>()(k.first) ^ hash<long long>()(k.second) ^ hash<long long>()(k.third);
+            }
+        };
+    };
+
+    Key generateHash(const string& input) {
+        Key result = {0, 0, 0};
+
+        for (char c : input) 
+        {
+            int charIndex = c - 'a';
+            int slotIndex = charIndex % 9;
+
+            switch (charIndex / 9) 
+            {
+                case 0:
+                    result.first += (1LL << (slotIndex * 7));
+                    break;
+                case 1:
+                    result.second += (1LL << (slotIndex * 7));
+                    break;
+                case 2:
+                    result.third += (1LL << (slotIndex * 7));
+                    break;
+            }
+        }
+
+        return result;
     }
 };
