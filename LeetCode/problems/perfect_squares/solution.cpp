d190e4f013feb1a811635a70070cb338c519627b
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-02-08 06:06:13 +0000

    Sync LeetCode submission - Perfect Squares (cpp)

--- LeetCode/problems/perfect_squares/solution.cpp
+++ LeetCode/problems/perfect_squares/solution.cpp
@@ -1,76 +1,27 @@
-/*class Solution {
-public:
-    int numSquares(int n) {
-        
-    }
-};*/
-
 class Solution {
-
 public:
-
-int numSquares(int n) {
-
-    int count=0;
-
-    queue<int> q;
-
-    q.push(n);
-
-    while(!q.empty()) {
-
-        int n=q.size();
-
-        for(int i=0; i<n; i++) {
-
-            int curr=q.front();
-
-            q.pop();
-
-            long long int a=sqroot(curr);
-
-            if(a*a==curr)
-
-                return count+1;
-
-            for(int j=a; j>0; j--) 
-
-                q.push(curr-j*j);
-
+    int numSquares(int n) 
+    {
+        int sqrtVal=(int)sqrt(n);
+        
+        if(sqrtVal*sqrtVal==n)
+            return 1;
+        
+        while(n%4==0)
+            n/=4;
+        
+        if(n%8==7)
+            return 4;
+        
+        for(int i=1;i*i<=n;i++)
+        {
+            int a=i*i;
+            int b=(int)sqrt(n-a);
+            
+            if(b*b==n-a)
+                return 2;
         }
-
-        count++;
-
+        
+        return 3;
     }
-
- return count;
-
-}
-
-long long int sqroot(int n) {
-
-    int low=0, high=n;
-
-    while(low<=high) {
-
-        long long int mid=low+(high-low)/2;
-
-        if(mid*mid==n)
-
-            return mid;
-
-        else if(mid*mid>n)
-
-            high=mid-1;
-
-        else
-
-            low=mid+1;
-
-    }
-
- return high;
-
-}
-
 };
