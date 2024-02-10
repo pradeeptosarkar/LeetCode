3906e6bc7996a1cc6434129c0c1ef2ef4ec5f1fe
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-02-10 14:22:03 +0000

    Sync LeetCode submission - Unique Paths II (cpp)

--- LeetCode/problems/unique_paths_ii/solution.cpp
+++ LeetCode/problems/unique_paths_ii/solution.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    int dp[101][101];
-    
-    int Solve(int i,int j,vector<vector<int>>& grid){
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
+    {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
         
-        if(i>=0 && j>=0 && grid[i][j]==1) return 0;
+        vector<vector<int>> dp(m, vector<int>(n));
         
-        if(i==0 && j==0) return 1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(obstacleGrid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                    
+                if(i==0 and j==0)
+                    dp[i][j]=1;
+                
+                else
+                {
+                    int up=0, left=0;
+                    
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    
+                    dp[i][j]=up+left;
+                }
+            }
+        }
         
-        if(i<0 || j<0) return 0;
-        
-        if(dp[i][j] !=-1) return dp[i][j]; 
-        
-         int left=Solve(i-1,j,grid);
-         int right=Solve(i,j-1,grid);
-        
-        return dp[i][j]=left+right;
-        
-    }
-    
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        
-        int n=grid.size();
-        int m=grid[0].size();
-        memset(dp,-1,sizeof(dp));
-        int ans=Solve(n-1,m-1,grid);
-        return ans;
-        
+        return dp[m-1][n-1];
     }
 };
