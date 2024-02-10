5a5aad49aec4f9f1a0699f7ec8277ab2d48a4873
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-02-10 17:24:28 +0000

    Sync LeetCode submission - Triangle (cpp)

--- LeetCode/problems/triangle/solution.cpp
+++ LeetCode/problems/triangle/solution.cpp
@@ -3,26 +3,11 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) 
     {
         int n=triangle.size();
-        vector<vector<int>>ans(n,vector<int>(n,0));
-        ans[0][0]=triangle[0][0];
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<triangle[i].size();j++)
-            {
-                if(j==0)
-                    ans[i][j]=ans[i-1][j]+triangle[i][j];
-                
-                else if(j==triangle[i].size()-1)
-                    ans[i][j]=ans[i-1][j-1]+triangle[i][j];
-                
-                else
-                    ans[i][j]=min(ans[i-1][j],ans[i-1][j-1])+triangle[i][j];
-            }
-        }
-        int soln=INT_MAX;
-        for(int i=0;i<n;i++)
-            soln=min(soln,ans[n-1][i]);
-        return soln;
         
+        for(int i=n-2;i>=0;i--)
+            for(int j=0;j<=i;j++)
+                triangle[i][j]+=min(triangle[i+1][j], triangle[i+1][j+1]);
+        
+        return triangle[0][0];
     }
 };
