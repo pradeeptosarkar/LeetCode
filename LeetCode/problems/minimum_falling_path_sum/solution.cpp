7cd136861b5a464bb9e1521b9e768d44fad7f90f
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-02-10 18:20:56 +0000

    Sync LeetCode submission - Minimum Falling Path Sum (cpp)

--- LeetCode/problems/minimum_falling_path_sum/solution.cpp
+++ LeetCode/problems/minimum_falling_path_sum/solution.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) 
     {
+        int ans=INT_MAX;
+        
         int n=matrix.size();
         
-        if(n>1)
+        for(int i=1;i<n;i++)
         {
-            for(int i=1;i<n;i++)
+            for(int j=0;j<n;j++)
             {
-                for(int j=0;j<n;j++)
-                {
-                    int a,b,c;
-                    a=(j==0?10000:matrix[i-1][j-1]);
-                    b=matrix[i-1][j];
-                    c=(j==n-1?10000:matrix[i-1][j+1]);
-                    matrix[i][j]+=min(a,min(b,c));
-                }
+                if(j==0)
+                    matrix[i][j]+=min(matrix[i-1][j], matrix[i-1][j+1]);
+                else if(j==n-1)
+                    matrix[i][j]+=min(matrix[i-1][j-1], matrix[i-1][j]);
+                else
+                    matrix[i][j]+=min(matrix[i-1][j-1], min(matrix[i-1][j], matrix[i-1][j+1]));
             }
         }
-        
-        int ans=INT_MAX;
         
         for(auto i:matrix[n-1])
             ans=min(ans,i);
