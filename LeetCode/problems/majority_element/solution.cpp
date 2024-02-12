92c5be80132af0038e8d812901c266c70cb3efd6
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-02-12 05:37:46 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- LeetCode/problems/majority_element/solution.cpp
+++ LeetCode/problems/majority_element/solution.cpp
@@ -2,34 +2,16 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) 
     {
-        int temp;
-        int cnt=0;
+        int maj=-1, count=0;
         
         for(int i=0;i<nums.size();i++)
         {
-            if(cnt==0)
-            {
-                temp=nums[i];
-                cnt+=1;
-            }
-                
-            else if(temp==nums[i])
-                cnt++;
+            if(count==0)
+                maj=nums[i];
             
-            else
-                cnt--;
+            count+=(nums[i]==maj ? 1 : -1);
         }
         
-        int ans=0;
-        
-        for(int i=0;i<nums.size();i++)
-            if(temp==nums[i])
-                ans++;
-        
-        if(ans>(nums.size()/2))
-            return temp;
-        
-        return -1;
-        
+        return maj;
     }
 };
