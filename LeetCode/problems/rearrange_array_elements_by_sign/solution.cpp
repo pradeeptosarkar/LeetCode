ae488d2ce68ae8efccc4d23548b527e3f4c20bc4
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-02-14 03:28:36 +0000

    Sync LeetCode submission - Rearrange Array Elements by Sign (cpp)

---
+++ LeetCode/problems/rearrange_array_elements_by_sign/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int> ans(n);
+        int pos=0, neg=1;
+        
+        for(int i:nums)
+        {
+            if(i>0)
+            {
+                ans[pos]=i;
+                pos+=2;
+            }
+            
+            else
+            {
+                ans[neg]=i;
+                neg+=2;
+            }
+        }
+        
+        return ans;
+    }
+};
