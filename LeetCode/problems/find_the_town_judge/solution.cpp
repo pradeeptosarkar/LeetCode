d74243f3df4727530f22258beffd5f101dab9ff2
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-02-22 13:11:32 +0000

    Sync LeetCode submission - Find the Town Judge (cpp)

--- LeetCode/problems/find_the_town_judge/solution.cpp
+++ LeetCode/problems/find_the_town_judge/solution.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) 
     {
-        vector<int> indegree(n+1,0), outdegree(n+1,0);
+        vector<int> degree(n+1,0);
         
         for(auto i:trust)
         {
-            outdegree[i[0]]++;
-            indegree[i[1]]++;
+            degree[i[0]]--;
+            degree[i[1]]++;
         }
         
         for(int i=1;i<=n;i++)
-        {
-            if(indegree[i]==n-1 and outdegree[i]==0)
+            if(degree[i]==n-1)
                 return i;
-        }
         
         return -1;
     }
