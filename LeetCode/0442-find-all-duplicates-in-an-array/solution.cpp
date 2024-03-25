9ea4f2478f42e86af8d218831b0f7690c71ff28b
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-03-25 07:04:23 +0000

    Sync LeetCode submission Runtime - 37 ms (67.69%), Memory - 35.8 MB (96.05%)

---
+++ LeetCode/0442-find-all-duplicates-in-an-array/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) 
+    {
+        vector<int> ans;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[abs(nums[i])-1]*=-1;
+            
+            if(nums[abs(nums[i])-1]>0)
+                ans.push_back(abs(nums[i]));
+        }
+        
+        return ans;
+    }
+};
