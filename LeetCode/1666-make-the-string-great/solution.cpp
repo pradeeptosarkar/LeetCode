45e6d053e9ea6ac6ba4dd6534aeeb508e5e89990
pradeeptosarkar <50446690+pradeeptosarkar@users.noreply.github.com>
2024-04-05 12:03:40 +0000

    Sync LeetCode submission Runtime - 4 ms (41.70%), Memory - 7.5 MB (97.38%)

---
+++ LeetCode/1666-make-the-string-great/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string makeGood(string s) 
+    {
+        for(int i=0;i<s.size();i++)
+            if(abs(s[i]-s[i+1])==32)
+                s.erase(i,2), i=max(i-2, -1);
+        
+        return s;
+    }
+};
